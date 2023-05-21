2303aff5dd1be369c1a21d3438f1164ce0e5effb
Jongdroid <qkr7627@gmail.com>
2023-05-21 18:41:28 +0900

    Create new Project

---
+++ app/src/main/java/com/jongdroid/jongdroid_family_talk/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.jongdroid.jongdroid_family_talk
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
